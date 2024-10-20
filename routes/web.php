055e2504e230ee8e8bf77ea6a99cfe1fe4853412
georgerabus <george.rabus@isa.utm.md>
2024-10-20 20:39:35 +0300

    routes organising

--- routes/web.php
+++ routes/web.php
@@ -6,29 +6,30 @@ use App\Http\Controllers\AnimeController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\UserController;
 
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+    
+    Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    Route::post('/comments-store', [CommentController::class, 'store'])->name('comments-store');
+    Route::post('/comments/reply/{id}', [CommentController::class, 'reply'])->name('comments-reply');
+    
+    Route::get('/profile', [UserController::class, 'showEditUser'])->name('user-profile');
+    Route::put('/profile/update', [UserController::class, 'editUser'])->name('profile-update');
+    
+    Route::post('/upload-photo', [UserController::class, 'uploadPhoto']);
+    Route::post('/save-cropped-image', [UserController::class, 'saveCroppedImage']);
+});
+
+// Public routes
 Route::get('/', [AnimeController::class, 'index'])->name('home');
-
 Route::get('/anime-list', [AnimeController::class, 'animeList'])->name('anime-list');
-
 Route::get('/contact', [AnimeController::class, 'contact'])->name('contact');
-
 Route::get('/search', [AnimeController::class, 'search'])->name('search');
-
-//for now
 Route::get('/anime', [AnimeController::class, 'animePage'])->name('animePage');
-
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login')->middleware('guest');
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
-
-Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register')->middleware('guest');
-Route::post('/register', [AuthController::class, 'register']);
-
-Route::post('/comments-store', [CommentController::class, 'store'])->name(('comments-store'))->middleware('auth');
-Route::post('/comments/reply/{id}', [CommentController::class, 'reply'])->name('comments-reply')->middleware('auth');
-
-Route::get('/profile', [UserController::class, 'showEditUser'])->name('user-profile')->middleware('auth');
-Route::put('/profile/update', [UserController::class, 'editUser'])->name('profile-update')->middleware('auth');
-Route::post('/upload-photo', [UserController::class, 'uploadPhoto']);
-Route::post('/save-cropped-image', [UserController::class, 'saveCroppedImage']);
-
